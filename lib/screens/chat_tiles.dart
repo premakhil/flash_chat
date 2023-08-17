e2ffd690b19e6ee7e4430b56b7494a8691db1039
Akhil Prem <akhilpchand@gmail.com>
2023-08-17 17:59:46 +0530

    Fetch function added

--- lib/screens/chat_tiles.dart
+++ lib/screens/chat_tiles.dart
@@ -9,7 +9,27 @@ class chatTiles extends StatefulWidget {
 }
 
 class _chatTilesState extends State<chatTiles> {
+  final _auth = FirebaseAuth.instance;
+  late User loggedinUser;
+
   @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser;
+
+      if (user != null) {
+        loggedinUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
